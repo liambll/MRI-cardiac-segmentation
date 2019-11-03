7d950fb04574ca13a6dd169c8b05ff5219b76f54
Liam Bui <liam.bui@terramera.com>
2019-11-03 09:35:12 -0800

    update unittest for utils/dataset.py

--- tests/unit/utils/test_dataset.py
+++ tests/unit/utils/test_dataset.py
@@ -25,5 +25,21 @@ class TestDataset(unittest.TestCase):
         expected = [([20, 40], [2, 4]), ([10, 30], [1, 3]), ([20, 40], [2, 4]), ([30, 10], [3, 1])]
         
         np.testing.assert_equal(generated_data, expected)
+        
+    def test_data_generator_loopback(self):
+        np.random.seed(13) # fix seed so that the randomization is fixed        
+        datasource = [('a', 10, 1), ('b', 20, 2), ('c', 30, 3), ('d', 40, 4)] # create a datasource
+
+        # generate data
+        generated_data = []
+        batch_size = 3
+        for i in range(1):
+            data_gen = dataset.data_generator(datasource, batch_size=batch_size, shuffle=True)
+            for j in range(len(datasource)//batch_size+1):
+                generated_data.append(data_gen.__next__())
+                
+        expected = [([20, 40, 10], [2, 4, 1]), ([30, 20, 40], [3, 2, 4])]
+        
+        np.testing.assert_equal(generated_data, expected)
 
 
